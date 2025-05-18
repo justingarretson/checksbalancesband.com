9699bf3cec1392650f6466ae471a27f176e39f6a
Justin Garretson <justingarretson@gmail.com>
2025-05-17 20:49:04 -0500

    First Commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<div class="container">
+            <div class="row d-flex flex-wrap align-items-center">
+                <div class="col-12 col-md-6">
+                    <!--<a href="#"><img src="img/core-img/logo.png" alt=""></a>-->
+                    <p class="copywrite-text"><a href="#"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved</p>
+                </div>
+
+                <div class="col-12 col-md-6">
+                    <div class="footer-nav">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="albums-store.php">Media</a></li>
+                            <li><a href="event.php">Events</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
---
+++ header.php
@@ -0,0 +1,42 @@
+
+       
+    <!-- Navbar Area -->
+         <div class="oneMusic-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="oneMusicNav">
+                       <!-- Nav brand -->                        
+                        <a href="index.php" class="nav-brand"><img src="img/LukeFoxTXTLogo.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                        <!-- Close Button -->
+                        <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+                           <!-- Nav Start -->
+                           <div class="classynav">
+                                <ul>
+                                    <li><a href="index.php">Home</a></li>
+                                    <li><a href="albums-store.php">Media</a></li>                                    
+                                    <li><a href="event.php">Events</a></li>
+                                    <!--<li><a href="blog.php">News</a></li>-->
+                                    <li><a href="contact.php">Contact</a></li>
+                                </ul>                            
+                                
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+                        
